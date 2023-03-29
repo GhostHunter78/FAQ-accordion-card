48c87340d9b5a022924ae52e578493d2cd48eda7
Luka Talakvadze <lukatalakvadze@gmail.com>
2023-03-30 03:46:33 +0400

    uploaded

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const answers = document.querySelectorAll(".answer");
 
 for (let i = 0; i < questions.length; i++) {
   questions[i].addEventListener("click", () => {
+    //hiding other answers
     for (let k = 0; k < answers.length; k++) {
       answers[k].style.display = "none";
     }
 
+    //show answer on click
     answers[i].style.display = "block";
   });
 }
